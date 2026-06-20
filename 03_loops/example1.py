e5daf0074c21eca32c486646f350837bf34fc7e5
Shima Asadi <shimaasadi.org3@gmail.com>
2026-06-21 02:33:34 +0330

    Update example1.py

--- 03_loops/example1.py
+++ 03_loops/example1.py
@@ -1,3 +1,4 @@
+#برنامه ای بنویسید که از کاربر 5 عدد دریافت کند و تعداد اعداد زوج میان آنها را در خروجی نمایش دهد.
 #Write a program that receives 5 numbers from the user and displays the number of even values among them.
 
 count=0
