b9dfc649be0038b6369f6c48137c982263290317
Shima Asadi <shimaasadi.org3@gmail.com>
2026-06-21 02:47:53 +0330

    Create example10.py

---
+++ 03_loops/example10.py
@@ -0,0 +1,5 @@
+#برنامه ای بنویسید که با استفاده از * یک مثلث با ارتفاع و قاعده ی 5 ایجاد کند.
+#Write a program that creates a triangle of height and base 5 using the '*' character.
+
+for i in range (5):
+    print(i*'*')
