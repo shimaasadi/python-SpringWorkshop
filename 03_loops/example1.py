b2f77253d5fb20b71a3c6660a8331b66250e1461
Shima Asadi <shimaasadi.org3@gmail.com>
2026-06-21 02:29:28 +0330

    Update and rename example.py to example1.py

---
+++ 03_loops/example1.py
@@ -0,0 +1,9 @@
+#Write a program that receives 5 numbers from the user and displays the number of even values among them.
+
+count=0
+for i in range(5):
+    x=int(input(f'plz enter number{i+1}'))
+    if x%2==0 :
+        count+=1
+
+print(f'there has been {count} even number in the list you entered')
