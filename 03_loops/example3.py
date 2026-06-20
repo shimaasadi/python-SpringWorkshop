f05c41a4ab4b7c45a0ee337cafbf1586e7464b32
Shima Asadi <shimaasadi.org3@gmail.com>
2026-06-21 02:34:41 +0330

    Create example3.py

---
+++ 03_loops/example3.py
@@ -0,0 +1,6 @@
+#برنامه ای بنویسید که اعداد فرد بین 20 تا 30 را در خروجی نمایش دهد.
+#Write a program that prints all odd numbers between 20 and 30.
+
+for i in range(20,30):
+    if i%2 != 0 :
+        print(i)
