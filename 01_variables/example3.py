ba0e1086c09a137f2d52bac47fd5baf0688c5550
Shima Asadi <shimaasadi.org3@gmail.com>
2026-06-21 03:44:50 +0330

    Delete 01_variables/example3.py

--- 01_variables/example3.py
+++
@@ -1,9 +0,0 @@
-#2- برنامه ای بنویسید که یک سال را از ورودی بگیرد و بررسی کند که آیا سال کبیسه است یا خیر
-#Write a program that receives a year and determines whether it is a leap year or not.
-
-year = int(input("plz enter the year"))
-
-if (year % 4 == 0 and year % 100 != 0):
-    print("this year is a leap year")
-else:
-    print("this year is not a leap year")
